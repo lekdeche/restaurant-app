3363d5c030611dc7ae2fe9d874e0e9aa2499b87a
lekdeche <ali.lekdech@gmail.com>
2021-06-10 15:13:06 +0200

    Ajout carouselle

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
-import CategoryCard from './components/CategoryCard/CategoryCard'
-import Navbar from './components/Navbar/Navbar'
-import ProductCard from './components/ProductCard/ProductCard'
-import { db } from './data/db'
+import { useState } from "react";
+import CategoryCard from "./components/CategoryCard/CategoryCard";
+import Navbar from "./components/Navbar/Navbar";
+import ProductCard from "./components/ProductCard/ProductCard";
+import { db } from "./data/db";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const App = () => {
-	return (
-		<div className='flex'>
-			<Navbar />
-			<main>
-				<div className='flex'>
-					{db.courses.map(course => (
-						<ProductCard
-							key={course.id}
-							name={course.name}
-							image={course.image}
-							description={course.description}
-							price={course.price}
-						/>
-					))}
-				</div>
-				<section>
-					<ul className='flex flex-wrap justify-around'>
-						{db.categories.map(category => (
-							<CategoryCard
-								key={category.id}
-								category={category.name}
-								image={category.image}
-								color={category.color}
-							/>
-						))}
-					</ul>
-				</section>
-			</main>
-		</div>
-	)
-}
+  // let isOpen = false ouverture fermeture du menu
 
-export default App
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => {
+    // je cherche à ouvrie le menu / à le rendre visible
+    setIsOpen(true);
+  };
+
+  return (
+    <div className="app">
+      {/*isOpen && <Navbar />*/}
+      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <main>
+        <section className="flex">
+          <button onClick={openMenu} className="ml-auto lg:hidden">
+            <img src="img/icons/menu.png" alt="Menu" className="h-10" />
+          </button>
+        </section>
+        <section>
+          <Swiper
+            spaceBetween={50}
+            slidesOffsetBefore={50}
+            slidesOffsetAfter={50}
+            slidesPerView={"auto"}
+          >
+            {db.courses.map((course) => (
+              <SwiperSlide>
+                <ProductCard
+                  key={course.id}
+                  name={course.name}
+                  image={course.image}
+                  description={course.description}
+                  price={course.price}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </section>
+
+        <section>
+          <ul className="flex flex-wrap justify-around">
+            {db.categories.map((category) => (
+              <CategoryCard
+                key={category.id}
+                category={category.name}
+                image={category.image}
+                color={category.color}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-const Navbar = () => {
+const Navbar = ({ isOpen, setIsOpen}) => {
+	const closeMenu = () => {
+		setIsOpen(false)
+	}
+	
 	return (
-		<nav className='w-80 flex-shrink-0 p-8 h-screen'>
+		<nav className={ isOpen ? 'navigation': 'navigation hide-navbar'}>
 			<h1 className='text-2xl uppercase tracking-wider mb-12'>Le couteau</h1>
 			<ul className='font-bold'>
 				<li className='mb-6'>Menu</li>
 				<li className='mb-6'>Commandes</li>
 				<li className='mb-6'>À propos</li>
 			</ul>
+			<button className='lg:hidden' onClick={closeMenu}>
+				<img className='w-24' src="img/icons/close.svg" alt="Fermer" />
+			</button>
 		</nav>
 	)
 }
